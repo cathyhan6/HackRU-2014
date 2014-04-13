74f4987f4fbc0a1142428e6f8914904b4e76cc65
Jake Silverman <JakeRossSilverman@gmail.com>
2014-04-13 01:55:30 -0400

    Created flask environment

--- wolframText.py
+++ wolframText.py
@@ -1,8 +1,18 @@
-from flask import Flask, request, redirect
+"""from flask import Flask, request, redirect
 import twilio.twiml
 
 app = Flask(Wolfram_sms)
 
 @app.route("/", methods=['GET, 'POST])
 def foo():
-	r
+	r"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
