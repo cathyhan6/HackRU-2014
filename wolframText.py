bb53d33fb07dd2cbae21ee5493962b9c9f95c03b
Jake Silverman <JakeRossSilverman@gmail.com>
2014-04-13 03:31:33 -0400

    Got it on Heroku

--- wolframText.py
+++ wolframText.py
@@ -6,23 +6,23 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def getTextData():
-	text = request.values.get('Body', none)
-	number = request.values.get('From', none)
+	text = request.values.get('Body', None)
+	number = request.values.get('From', None)
 
 	# todo Wolfram api stuff
 
-	account_sid = "PN88dd7bd53b53c71f7dd274e5039265ad"
+	account_sid = "ACc9712f9632cb63e46876bc4a62e476af"
 	auth_token = "a0f08f7a8d0870df130ee070b59009f0"
 	client = TwilioRestClient(account_sid, auth_token)
 
 	sms = client.sms.messages.create(
 		body = "sent success", 
 		to = number, 
-		from_= "+3477325742"
+		from_= "+13477325742"
 		)
+	return "Success"
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug=True)
 
