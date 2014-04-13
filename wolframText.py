eb2b0fdc4295fa8c669022ee59a29c61a2fb8f4a
Jake Silverman <JakeRossSilverman@gmail.com>
2014-04-13 06:42:53 -0400

    sendgrid help

--- wolframText.py
+++ wolframText.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, jsonify
 import sendgrid
+import json
 import wolframalpha
 
 app = Flask(__name__)
@@ -7,22 +8,24 @@ clientW = wolframalpha.Client("TTAVEX-73R7X8KQ9V")
 clientSG = sendgrid.SendGridClient("JakeRossSilverman", "cornell01")
 
 
-@app.route("/wolf", methods=['GET', 'POST'])
+@app.route("/", methods=['POST'])
 def getTextData():
 	text = request.values.get("text", None)
-	userEmail = request.values.get("from", None)
+	userEmail = json.loads(request.values.get("envelope", None))["from"]
 	ourEmail = request.values.get("to", None)
 
 	res = clientW.query(text)
 	returnValue = next(res.results).text
-	
 	message = sendgrid.Mail()
 	message.add_to(userEmail)
+	print userEmail
+	print ourEmail
 	message.set_subject('Result')
 	message.set_text(returnValue)
 	message.set_from('AlphaText <' + ourEmail + '>')
-	status, msg = sg.send(message)
+	print clientSG.send(message)
 
+	return jsonify({})
 
 if __name__ == '__main__':
     app.run(debug=True)
