2393e776756319318050a3675f374b54e63002a5
Cathy Han <ch659@cornell.edu>
2014-04-13 02:59:24 -0400

    Receiving/sending SMS

--- wolframText.py
+++ wolframText.py
@@ -1,18 +1,27 @@
-"""from flask import Flask, request, redirect
+from flask import Flask, request, redirect
 import twilio.twiml
+from twilio.rest import TwilioRestClient
 
-app = Flask(Wolfram_sms)
-
-@app.route("/", methods=['GET, 'POST])
-def foo():
-	r"""
-
-from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/", methods=['GET, 'POST])
+def getTextData():
+	text = request.values.get('Body', none)
+	number = request.values.get('From', none)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+	# todo Wolfram api stuff
+
+	account_sid = "PN88dd7bd53b53c71f7dd274e5039265ad"
+	auth_token = "a0f08f7a8d0870df130ee070b59009f0"
+	client = TwilioRestClient(account_sid, auth_token)
+
+	sms = client.sms.messages.create(
+		body = "sent success", 
+		to = number, 
+		from_= "+3477325742"
+		)
+
+
+if __name__ == '__main__':
+    app.run()
+
