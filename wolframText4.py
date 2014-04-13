4dba47e4a2e798dd8d73a7c98d1ccc74837f1544
Cathy Han <ch659@cornell.edu>
2014-04-13 08:55:16 -0400

    new parsing

---
+++ wolframText4.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, redirect, jsonify
+import sendgrid
+import json
+import wolframalpha
+import urllib
+import sys
+
+app = Flask(__name__)
+clientW = wolframalpha.Client("TTAVEX-73R7X8KQ9V")
+clientSG = sendgrid.SendGridClient("JakeRossSilverman", "cornell01")
+
+
+@app.route("/", methods=['POST'])
+def getTextData():
+	envelope = json.loads(request.form.get('envelope'))
+	text = request.form.get("text")
+	userEmail = envelope['to'][0]
+	ourEmail = envelope['from']
+
+	appid = "TTAVEX-73R7X8KQ9V"
+	query = text
+	w = wolfram(appid)
+	w.search(query)
+
+	"""print returnValue
+	urllib.urlretrieve(returnValue, 'data.png') """
+	message = sendgrid.Mail()
+	message.add_to(userEmail)
+	print userEmail
+	print ourEmail
+	message.set_subject('Result')
+	message.set_text('success')
+	message.set_from('AlphaText <' + ourEmail + '>')
+	print clientSG.send(message)
+
+	return jsonify({})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
