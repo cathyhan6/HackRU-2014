e820ee0aad39e212dd0bed2a9fa8f3792e83661a
Cathy Han <ch659@cornell.edu>
2014-04-13 03:39:59 -0400

    Incorporated Wolfram API

--- wolframText.py
+++ wolframText.py
@@ -1,22 +1,24 @@
 from flask import Flask, request, redirect
 import twilio.twiml
 from twilio.rest import TwilioRestClient
+import wolframalpha
 
 app = Flask(__name__)
+clientW = wolframalpha.Client("TTAVEX-73R7X8KQ9V")
 
 @app.route("/", methods=['GET, 'POST])
-def getTextData():
+def doEverything():
 	text = request.values.get('Body', none)
 	number = request.values.get('From', none)
 
-	# todo Wolfram api stuff
+	result = clientW.query(text)
 
 	account_sid = "PN88dd7bd53b53c71f7dd274e5039265ad"
 	auth_token = "a0f08f7a8d0870df130ee070b59009f0"
-	client = TwilioRestClient(account_sid, auth_token)
+	clientT = TwilioRestClient(account_sid, auth_token)
 
-	sms = client.sms.messages.create(
-		body = "sent success", 
+	sms = clientT.sms.messages.create(
+		body = result, 
 		to = number, 
 		from_= "+3477325742"
 		)
